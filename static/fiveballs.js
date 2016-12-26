5798b47bf5a70a3f718cc40cda08de1e3f45ae27
Van <vango85@gmail.com>
2016-12-26 02:31:38 +0100

    [events] Added onclick stuff

--- static/fiveballs.js
+++ static/fiveballs.js
@@ -59,15 +59,13 @@ function Board(stage) {
     this.tileTexture = new PIXI.Texture.fromImage("static/tile.png")
     this.ballTexture = new PIXI.Texture.fromImage("static/ball.png")
     this.stage = stage;
+    this.selectedBall = null;
     this.init = function() {
         // Init tiles
         for (var i = 0; i < numCells; i++) {
             this.tileMatrix = new Array(numCells);
             for (var j = 0; j < numCells; j++) {
-                var tile = new PIXI.Sprite(this.tileTexture);
-                tile.x = borderSize + i * cellSize;
-                tile.y = borderSize + j * cellSize;
-                this.stage.addChild(tile);
+              this.addTile(i,j);
             }
         }
 
@@ -75,30 +73,51 @@ function Board(stage) {
         for (var i = 0; i < numCells; i++) {
             this.ballMatrix[i] = new Array(numCells);
         }
-        
+
         // Place first balls
         var count = numStartBalls;
-        while(count > 0){
-          var x = Math.floor(Math.random()*numCells);
-          var y = Math.floor(Math.random()*numCells);
-          if(this.ballMatrix[x][y] != null){
-            continue;
-          }
-          
-          var color = Math.floor(Math.random()*numBallTypes);
-          this.addBall(x,y, color);
-          count = count - 1;
+        while (count > 0) {
+            var x = Math.floor(Math.random() * numCells);
+            var y = Math.floor(Math.random() * numCells);
+            if (this.ballMatrix[x][y] != null) {
+                continue;
+            }
+
+            var color = Math.floor(Math.random() * numBallTypes);
+            this.addBall(x, y, color);
+            count = count - 1;
         }
-        
+
+    }
+
+    this.addTile = function(x, y) {
+        var board = this;
+        var tileSprite = new PIXI.Sprite(this.tileTexture);
+        var tile = new Tile(x, y, tileSprite);
+        tileSprite.x = borderSize + x * cellSize;
+        tileSprite.y = borderSize + y * cellSize;
+        tileSprite.interactive = true;
+        tileSprite.on('mouseup', function(mousedata) {
+            board.tileClicked(tile);
+        });
+        this.stage.addChild(tileSprite);
     }
 
     this.addBall = function(x, y, colorIndex) {
         var ballSprite = new PIXI.Sprite(this.ballTexture);
-        ballSprite.x = borderSize + x * cellSize;
-        ballSprite.y = borderSize + y * cellSize;
+        ballSprite.x = this.getCoordsForCell(x, y)[0];
+        ballSprite.y = this.getCoordsForCell(x, y)[1];
         ballSprite.tint = colorMap[colorIndex];
-        this.ballMatrix[x][y] = new Ball(x, y, ballSprite);
+        var ball = new Ball(x, y, ballSprite);
+
+        this.ballMatrix[x][y] = ball;
         this.stage.addChild(ballSprite);
+    }
+
+    this.moveBall = function(ball, x, y) {
+        var coords = this.getCoordsForCell(x, y);
+        ball.ballSprite.x = coords[0];
+        ball.ballSprite.y = coords[1];
     }
 
     this.removeBall = function(ball) {
@@ -106,10 +125,36 @@ function Board(stage) {
         this.ballMatrix[ball.x,
             ball.y] = null;
     }
+
+    this.tileClicked = function(tile) {
+        var ball = this.ballMatrix[tile.x][tile.y];
+        if (ball != null) {
+            console.log("From board, ball clicked : " + ball.x + " , " + ball.y);
+        } else {
+
+            console.log("From board, tiled clicked : " + tile.x + " , " + tile.y);
+        }
+    }
+
+    this.getCoordsForCell = function(x, y) {
+        return [
+            borderSize + x * cellSize,
+            borderSize + y * cellSize
+        ];
+    }
+}
+
+function Tile(x, y, sprite) {
+    this.x = x;
+    this.y = y;
+    this.sprite = sprite;
 }
 
 function Ball(x, y, colorIndex) {
     this.x = x;
     this.y = y;
     this.colorIndex = colorIndex;
+
+    this.onClick = function(mouseEvent) {}
+
 }
